fdc6961e4520da3db857cce23efa9da89813ae7e
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2023-01-13 04:18:01 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,8 +29,9 @@ function __build_c
     $CC $CFLAGS $DRIVERSDIR/keyboard.c -o $BUILDDIR/keyboard.c.o
     $CC $CFLAGS $DRIVERSDIR/mouse.c -o $BUILDDIR/mouse.c.o
     $CC $CFLAGS $DRIVERSDIR/acpi.c -o $BUILDDIR/acpi.c.o
-    $CC $CFLAGS $DRIVERSDIR/cmos.c -o $BUILDDIR/cmos.c.o
     $CC $CFLAGS $DRIVERSDIR/pic.c -o $BUILDDIR/pic.c.o
+    $CC $CFLAGS $DRIVERSDIR/ata.c -o $BUILDDIR/ata.c.o
+    $CC $CFLAGS $DRIVERSDIR/cmos.c -o $BUILDDIR/cmos.c.o
 
     $CC $CFLAGS $KERNELDIR/string.c -o $BUILDDIR/string.c.o
     $CC $CFLAGS $KERNELDIR/print.c -o $BUILDDIR/print.c.o
@@ -59,7 +60,7 @@ function __build_s
 
 function __link_k
 {
-    ld -m elf_i386 -Tconfig/linker.ld -o $BUILDDIR/kernel.elf $BUILDDIR/boot.asm.o $BUILDDIR/kernel.c.o $BUILDDIR/userspace.c.o $BUILDDIR/time.c.o $BUILDDIR/vga.c.o $BUILDDIR/string.c.o $BUILDDIR/print.c.o $BUILDDIR/vesa.c.o  $BUILDDIR/pio.c.o $BUILDDIR/isr.c.o $BUILDDIR/idt.c.o $BUILDDIR/syscalls.c.o $BUILDDIR/kgdt.c.o $BUILDDIR/interrupts.asm.o $BUILDDIR/gdt.asm.o $BUILDDIR/pmm.c.o $BUILDDIR/init.c.o $BUILDDIR/memory.c.o $BUILDDIR/keyboard.c.o $BUILDDIR/mouse.c.o $BUILDDIR/stdlib.c.o $BUILDDIR/acpi.c.o $BUILDDIR/kpanic.c.o $BUILDDIR/vfs.c.o $BUILDDIR/cmos.c.o $BUILDDIR/kmode.c.o $BUILDDIR/pic.c.o $BUILDDIR/idt.asm.o $BUILDDIR/syscalls.asm.o $BUILDDIR/irq.asm.o $BUILDDIR/bios32_call.asm.o
+    ld -m elf_i386 -Tconfig/linker.ld -o $BUILDDIR/kernel.elf $BUILDDIR/boot.asm.o $BUILDDIR/kernel.c.o $BUILDDIR/userspace.c.o $BUILDDIR/cmos.c.o $BUILDDIR/vga.c.o $BUILDDIR/string.c.o $BUILDDIR/print.c.o $BUILDDIR/vesa.c.o  $BUILDDIR/pio.c.o $BUILDDIR/isr.c.o $BUILDDIR/idt.c.o $BUILDDIR/syscalls.c.o $BUILDDIR/kgdt.c.o $BUILDDIR/interrupts.asm.o $BUILDDIR/gdt.asm.o $BUILDDIR/pmm.c.o $BUILDDIR/init.c.o $BUILDDIR/memory.c.o $BUILDDIR/keyboard.c.o $BUILDDIR/mouse.c.o $BUILDDIR/stdlib.c.o $BUILDDIR/acpi.c.o $BUILDDIR/kpanic.c.o $BUILDDIR/vfs.c.o $BUILDDIR/kmode.c.o $BUILDDIR/pic.c.o $BUILDDIR/idt.asm.o $BUILDDIR/irq.asm.o $BUILDDIR/ata.c.o
 }
 
 function __gen_iso
@@ -78,4 +79,4 @@ __build_s
 __link_k
 __gen_iso
 
-qemu-system-x86_64 -cdrom ./build/quantumos.iso -m 3G -vga vmware -serial stdio
+qemu-system-x86_64 -cdrom ./build/quantumos.iso -drive file=ext2.img,format=raw -m 3G -vga vmware -serial stdio
