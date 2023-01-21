fac0afdbfc9a0d10556b9d59f3af431d5cc64c85
SolindekDev <solindeklive.biznes@gmail.com>
2023-01-21 18:22:29 +0100

    Created new folder sys/x86_64

--- userspace/userspace.c
+++ userspace/userspace.c
@@ -20,24 +20,12 @@
 
 #include <fs/vfs.h>
 
-void task1()
-{
-    printf("Task 1\n");
-}
-
-void task2()
-{
-    printf("Task 2 that will be forked\n");
-    fork();
-}
-
 void userspace_initialize(void) 
 {
     vesa_clear();
     set_default_pos();
 
-    process_spawn(task1, 0, 1000, 0, 1);
-    process_spawn(task2, 0, 1000, 0, 1);
+    // Load GUI and go on!
 
     for (;;) {}
 }
